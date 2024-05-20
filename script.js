f09b1ab49d0a6439fa73859881683ceb67e9a535
Miguel <22322402+mpuche3@users.noreply.github.com>
2024-05-20 08:33:12 +0000

    message

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// git add .
+// git commit -m "message"
+// git push
+// git status
 
 const xhr = new XMLHttpRequest();
 const url = "tracks.json";
@@ -6,6 +10,7 @@ xhr.send();
 const tracks = JSON.parse(xhr.responseText);
 
 const FactoryAudio = function () {
+    const playbackRate = 0.8
     let audio = document.createElement('audio'); 
     let itracks = 0;
     let iCurrentTimes = 0;
@@ -36,13 +41,14 @@ const FactoryAudio = function () {
         // then audio.currentTime will be set to zero.
         audio.src = audioFileFullPath;
         audio.currentTime = currentTime;
+        audio.playbackRate = playbackRate; // Add this line
         promisePlay = audio.play();
         status = "PLAYING";
         timeoutId_pause = setTimeout(_ => {
             promisePlay.then(_ => {
                 audio.pause();
             });
-        }, duration * 1000)
+        }, duration * 1000 * (1 / playbackRate) + 1000)
         const additionalTimeintheloop = 1000;
         timeoutId_play = setTimeout(_ => {
             play();
@@ -90,12 +96,6 @@ const FactoryAudio = function () {
         play();
     }
 
-    // document.addEventListener('swiped-right', playPrevious);
-    // document.addEventListener('swiped-left', playNext);
-    // document.addEventListener('swiped-up', previousTrack);
-    // document.addEventListener('swiped-down', nextTrack);
-    // document.addEventListener("click", pause_play);
-
     document.querySelector(".button1").addEventListener("click", playNext)
     document.querySelector(".button2").addEventListener("click", playPrevious)    
     document.querySelector(".button3").addEventListener("click", previousTrack)
@@ -116,178 +116,4 @@ const FactoryAudio = function () {
     return {audio, playPrevious, playNext, pause_play, play, getStatus, nextTrack, previousTrack}
 }
 
-// const href = window.location.href
-// const alternative_track = href.split("=").pop()
-// if (alternative_track !== href) track = alternative_track
 const mpa = FactoryAudio()
-
-const bbb = 3;
-
-
-
-
-
-
-
-
-
-
-
-
-/*!
- * swiped-events.js - v@version@
- * Pure JavaScript swipe events
- * https://github.com/john-doherty/swiped-events
- * @inspiration https://stackoverflow.com/questions/16348031/disable-scrolling-when-touch-moving-certain-element
- * @author John Doherty <www.johndoherty.info>
- * @license MIT
- */
-
-(function (window, document) {
-    'use strict';
-
-    // patch CustomEvent to allow constructor creation (IE/Chrome)
-    if (typeof window.CustomEvent !== 'function') {
-
-        window.CustomEvent = function (event, params) {
-
-            params = params || { bubbles: false, cancelable: false, detail: undefined };
-
-            var evt = document.createEvent('CustomEvent');
-            evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
-            return evt;
-        };
-
-        window.CustomEvent.prototype = window.Event.prototype;
-    }
-
-    document.addEventListener('touchstart', handleTouchStart, false);
-    document.addEventListener('touchmove', handleTouchMove, false);
-    document.addEventListener('touchend', handleTouchEnd, false);
-
-    var xDown = null;
-    var yDown = null;
-    var xDiff = null;
-    var yDiff = null;
-    var timeDown = null;
-    var startEl = null;
-
-    /**
-     * Fires swiped event if swipe detected on touchend
-     * @param {object} e - browser event object
-     * @returns {void}
-     */
-    function handleTouchEnd(e) {
-
-        // if the user released on a different target, cancel!
-        if (startEl !== e.target) return;
-
-        var swipeThreshold = parseInt(getNearestAttribute(startEl, 'data-swipe-threshold', '20'), 10); // default 20px
-        var swipeTimeout = parseInt(getNearestAttribute(startEl, 'data-swipe-timeout', '500'), 10);    // default 500ms
-        var timeDiff = Date.now() - timeDown;
-        var eventType = '';
-        var changedTouches = e.changedTouches || e.touches || [];
-
-        if (Math.abs(xDiff) > Math.abs(yDiff)) { // most significant
-            if (Math.abs(xDiff) > swipeThreshold && timeDiff < swipeTimeout) {
-                if (xDiff > 0) {
-                    eventType = 'swiped-left';
-                }
-                else {
-                    eventType = 'swiped-right';
-                }
-            }
-        }
-        else if (Math.abs(yDiff) > swipeThreshold && timeDiff < swipeTimeout) {
-            if (yDiff > 0) {
-                eventType = 'swiped-up';
-            }
-            else {
-                eventType = 'swiped-down';
-            }
-        }
-
-        if (eventType !== '') {
-
-            var eventData = {
-                dir: eventType.replace(/swiped-/, ''),
-                touchType: (changedTouches[0] || {}).touchType || 'direct',
-                xStart: parseInt(xDown, 10),
-                xEnd: parseInt((changedTouches[0] || {}).clientX || -1, 10),
-                yStart: parseInt(yDown, 10),
-                yEnd: parseInt((changedTouches[0] || {}).clientY || -1, 10)
-            };
-
-            // fire `swiped` event event on the element that started the swipe
-            startEl.dispatchEvent(new CustomEvent('swiped', { bubbles: true, cancelable: true, detail: eventData }));
-
-            // fire `swiped-dir` event on the element that started the swipe
-            startEl.dispatchEvent(new CustomEvent(eventType, { bubbles: true, cancelable: true, detail: eventData }));
-        }
-
-        // reset values
-        xDown = null;
-        yDown = null;
-        timeDown = null;
-    }
-
-    /**
-     * Records current location on touchstart event
-     * @param {object} e - browser event object
-     * @returns {void}
-     */
-    function handleTouchStart(e) {
-
-        // if the element has data-swipe-ignore="true" we stop listening for swipe events
-        if (e.target.getAttribute('data-swipe-ignore') === 'true') return;
-
-        startEl = e.target;
-
-        timeDown = Date.now();
-        xDown = e.touches[0].clientX;
-        yDown = e.touches[0].clientY;
-        xDiff = 0;
-        yDiff = 0;
-    }
-
-    /**
-     * Records location diff in px on touchmove event
-     * @param {object} e - browser event object
-     * @returns {void}
-     */
-    function handleTouchMove(e) {
-
-        if (!xDown || !yDown) return;
-
-        var xUp = e.touches[0].clientX;
-        var yUp = e.touches[0].clientY;
-
-        xDiff = xDown - xUp;
-        yDiff = yDown - yUp;
-    }
-
-    /**
-     * Gets attribute off HTML element or nearest parent
-     * @param {object} el - HTML element to retrieve attribute from
-     * @param {string} attributeName - name of the attribute
-     * @param {any} defaultValue - default value to return if no match found
-     * @returns {any} attribute value or defaultValue
-     */
-    function getNearestAttribute(el, attributeName, defaultValue) {
-
-        // walk up the dom tree looking for attributeName
-        while (el && el !== document.documentElement) {
-
-            var attributeValue = el.getAttribute(attributeName);
-
-            if (attributeValue) {
-                return attributeValue;
-            }
-
-            el = el.parentNode;
-        }
-
-        return defaultValue;
-    }
-
-}(window, document));
